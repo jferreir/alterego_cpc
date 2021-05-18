fa5065b1e1a00908472dfc2f7c258f43498dcc93
jferreir <jesus.ferreira@gmail.com>
2021-05-18 14:36:41 +0200

    Added tiled project

---
+++ src/tiled/CPC_FONT_MODE1_8X8.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="CPC_FONT_MODE1_8X8" tilewidth="8" tileheight="8" tilecount="112" columns="28">
+ <image source="CPC_FONT_MODE1_8X8.bmp" width="224" height="32"/>
+</tileset>
