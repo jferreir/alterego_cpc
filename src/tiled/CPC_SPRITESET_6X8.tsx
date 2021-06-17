74a9beb57e99e471db5b6ede346d53e773c0af36
zogo <zogo@linux.fritz.box>
2021-06-17 08:20:00 +0200

    Add dir location for individual tools

--- src/tiled/CPC_SPRITESET_6X8.tsx
+++ src/tiled/CPC_SPRITESET_6X8.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.6.0" name="CPC_SPRITESET_6X8" tilewidth="6" tileheight="8" tilecount="80" columns="20">
- <image source="CPC_SPRITESET_6X8.bmp" trans="ff00ff" width="120" height="32"/>
+	<image source="../CPC_SPRITESET_6X8.bmp" trans="ff00ff" width="120" height="32"/>
 </tileset>
--- src/tiled/CPC_TILESET_6X8.tsx
+++ src/tiled/CPC_TILESET_6X8.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.6.0" name="CPC_TILESET_6X8" tilewidth="6" tileheight="8" tilecount="195" columns="13">
- <image source="CPC_TILESET_6X8.bmp" width="78" height="120"/>
+	<image source="../CPC_TILESET_6X8.bmp" width="78" height="120"/>
 </tileset>
